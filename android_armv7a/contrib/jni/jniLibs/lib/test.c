c11c436325226ef42085bcd94d0f0d5f310f2a4d
hao.yu <hao.yu@ushow.media>
2020-12-18 14:33:08 +0800

    delele build output libs

--- android_armv7a/contrib/jni/jniLibs/lib/test.c
+++
@@ -1,6 +0,0 @@
-  #include <openssl/ssl.h>
-  #include <stdint.h>
-    long check_OPENSSL_init_ssl(void) { return (long) OPENSSL_init_ssl; }
-    int main(void) { int ret = 0;
-    ret |= ((intptr_t)check_OPENSSL_init_ssl) & 0xFFFF;
-   return ret; }
