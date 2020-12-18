723329509f8f4ab6d59e5bfd15d9818bf2039786
hao.yu <hao.yu@ushow.media>
2020-12-18 14:45:06 +0800

    delele build output libs

--- android_armv7a/contrib/jni/jniLibs/armeabi-v7a/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/shared/ytxplayerlib/ytxPlayer/android/contrib/jni/jniLibs/armeabi-v7a/lib"
-XML2_LIBS="-lxml2 -lz     -lm "
-XML2_INCLUDEDIR="-I/shared/ytxplayerlib/ytxPlayer/android/contrib/jni/jniLibs/armeabi-v7a/include/libxml2"
-MODULE_VERSION="xml2-2.9.10"
-
