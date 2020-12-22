6ac3f02c202a6466082a5a058c8eba0d6ff26d1c
jack <ytxhao@gmail.com>
2020-12-22 17:18:38 +0800

    add libpng build file

---
+++ android_arm64-v8a/contrib/libpng/libpng-1.6.37/build_android_libpng.sh
@@ -0,0 +1,44 @@
+#libpng
+export ANDROID_NDK_HOME=/home/vagrant/Android/android-ndk-r21d
+YTXPLAYER_PATH=$(git rev-parse --show-toplevel)
+HOST_TAG=linux-x86_64
+API=26
+# Application Binary Interface (ABI)
+ABI=arm64-v8a
+ARCH=arm64
+CPU=armv8-a
+TARGET=aarch64-linux-android
+PREBUILT=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt
+PREFIX=$YTXPLAYER_PATH/build/output/jniLibs/$ABI
+
+PLATFORM=$ANDROID_NDK_HOME/platforms/android-$API/arch-$ARCH/
+TOOLCHAIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_TAG
+
+echo $PREFIX
+     
+CFLAGS="-fpic -DANDROID -ffunction-sections -msoft-float -lm"
+
+CROSS_COMPILE=$PREBUILT/$HOST_TAG/bin/${TARGET}${API}-
+
+LDFLAGS=-L${PREFIX}/lib
+
+export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
+export PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig
+
+export CXX="${CROSS_COMPILE}clang++ --sysroot=${TOOLCHAIN}/sysroot"
+export CC="${CROSS_COMPILE}clang --sysroot=${TOOLCHAIN}/sysroot"
+
+export NM="${PREBUILT}/$HOST_TAG/bin/${TARGET}-nm"
+export STRIP="$PREBUILT/$HOST_TAG/bin/${TARGET}-strip"
+export RANLIB="$PREBUILT/$HOST_TAG/bin/${TARGET}-ranlib"
+export AR="$PREBUILT/$HOST_TAG/bin/${TARGET}-ar"
+export LD="$PREBUILT/$HOST_TAG/bin/${TARGET}-ld"
+
+./configure --prefix=$PREFIX \
+--host=arm-linux-androideabi \
+
+make
+make install
+# cp .libs/libpng.so  .libs/libpng.a ${PREFIX}/lib
+# rm ${PREFIX}/lib/pkgconfig/libpng.pc
+# cp libpng.pc ${PREFIX}/lib/pkgconfig
