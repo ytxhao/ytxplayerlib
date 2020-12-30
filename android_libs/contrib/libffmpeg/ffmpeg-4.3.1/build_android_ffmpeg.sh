5321b21bb79a8d4bf77af833b1d796b51a51da54
jack <ytxhao@gmail.com>
2020-12-30 19:05:53 +0800

    update ffmpeg build file and README.TXT

--- android_libs/contrib/libffmpeg/ffmpeg-4.3.1/build_android_ffmpeg.sh
+++ android_libs/contrib/libffmpeg/ffmpeg-4.3.1/build_android_ffmpeg.sh
@@ -5,10 +5,79 @@ HOST_TAG=linux-x86_64
 
 API=26
 # Application Binary Interface (ABI)
-ABI=arm64-v8a
-ARCH=arm64
-CPU=armv8-a
-TARGET=aarch64-linux-android
+ABI=
+ARCH=
+TARGET=
+TARGET_TOOL=
+HOST=
+
+fun_build_clean() {
+  echo "build target clean"
+  make clean
+  make distclean
+}
+
+fun_build_config_armv7a() {
+  echo "build target armv7a config"
+  # Application Binary Interface (ABI)
+  ABI=armeabi-v7a
+  ARCH=arm
+  TARGET=armv7a-linux-androideabi
+  TARGET_TOOL=arm-linux-androideabi
+  HOST=arm-linux-android
+}
+
+fun_build_config_arm64() {
+  echo "build target arm64 config"
+  # Application Binary Interface (ABI)
+  ABI=arm64-v8a
+  ARCH=arm64
+  TARGET=aarch64-linux-android
+  TARGET_TOOL=aarch64-linux-android
+  HOST=arm-linux-android
+}
+
+fun_build_config_x86() {
+  echo "build target x86 config"
+  # Application Binary Interface (ABI)
+  ABI=x86
+  ARCH=x86
+  TARGET=i686-linux-android
+  TARGET_TOOL=i686-linux-android
+  HOST=x86-linux-android
+}
+
+fun_build_config_x86_64() {
+  echo "build target x86_64 config"
+  # Application Binary Interface (ABI)
+  ABI=x86_64
+  ARCH=x86_64
+  TARGET=x86_64-linux-android
+  TARGET_TOOL=x86_64-linux-android
+  HOST=x86-linux-android
+}
+
+if [ "$1" = "clean" ]; then
+  fun_build_clean
+  exit 0
+fi
+
+if [ "$1" = "-armv7a" ] || [ "$2" = "-armv7a" ]; then
+  fun_build_config_armv7a
+fi
+
+if [ "$1" = "-arm64" ] || [ "$2" = "-arm64" ]; then
+  fun_build_config_arm64
+fi
+
+if [ "$1" = "-x86" ] || [ "$2" = "-x86" ]; then
+  fun_build_config_x86
+fi
+
+if [ "$1" = "-x86_64" ] || [ "$2" = "-x86_64" ]; then
+  fun_build_config_x86_64
+fi
+
 
 PREBUILT=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt
 PREFIX=$YTXPLAYER_PATH/build/output/jniLibs/$ABI
@@ -18,52 +87,7 @@ CROSS_COMPILE=$PREBUILT/$HOST_TAG/bin/${TARGET}${API}-
 
 SYSROOT=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_TAG/sysroot
 
-# OPTIMIZE_CFLAGS=
-#HOST_TAG 的值根据系统修改
-#macOS  darwin-x86_64
-#Linux   linux-x86_64
-#32-bit Windows  windows
-#64-bit Windows  windows-x86_64
-
-
-# arm64-v8a   aarch64-linux-android
-
-# ARCH=arm64
-# CPU=armv8-a
-# TARGET=aarch64-linux-android
-# STRIP=aarch64-linux-android
-OPTIMIZE_CFLAGS="-march=$CPU"
-
-
-# armeabi-v7a armv7a-linux-androideabi
-
-# STRIP=arm-linux-androideabi
-# OPTIMIZE_CFLAGS="-mfloat-abi=softfp -mfpu=vfp -marm -march=$CPU "
-
-# export AR=$TOOLCHAIN/bin/$TARGET-ar
-# export AS=$TOOLCHAIN/bin/$TARGET-as
-# export CC=$TOOLCHAIN/bin/$TARGET$API-clang
-# export CXX=$TOOLCHAIN/bin/$TARGET$API-clang++
-# export LD=$TOOLCHAIN/bin/$TARGET-ld
-# export RANLIB=$TOOLCHAIN/bin/$TARGET-ranlib
-# export STRIP=$TOOLCHAIN/bin/$TARGET-strip
-
-# # x86 i686-linux-android
-
-# ARCH=x86
-# CPU=x86
-# TARGET=i686-linux-android
-# STRIP=i686-linux-android
-# OPTIMIZE_CFLAGS="-march=i686 -mtune=intel -mssse3 -mfpmath=sse -m32"
-
-# x86-64  x86_64-linux-android
-
-# ARCH=x86_64
-# CPU=x86-64
-# TARGET=x86_64-linux-android
-# STRIP=x86_64-linux-android
-# OPTIMIZE_CFLAGS="-march=$CPU -msse4.2 -mpopcnt -m64 -mtune=intel"
-
+#OPTIMIZE_CFLAGS="-march=$CPU"
 
 export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
 export PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig
@@ -71,8 +95,8 @@ export PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig
 export CXX="${CROSS_COMPILE}clang++"
 export CC="${CROSS_COMPILE}clang"
 # NM="${PREBUILT}/${HOST_TAG}/bin/arm-linux-androideabi-nm"
-export STRIP="$PREBUILT/$HOST_TAG/bin/${TARGET}-strip"
-export RANLIB="$PREBUILT/$HOST_TAG/bin/${TARGET}-ranlib"
+export STRIP="$PREBUILT/$HOST_TAG/bin/${TARGET_TOOL}-strip"
+export RANLIB="$PREBUILT/$HOST_TAG/bin/${TARGET_TOOL}-ranlib"
 # AR="$PREBUILT/${HOST_TAG}/bin/arm-linux-androideabi-ar"
 # 注意这里不能使用arm-linux-androideabi-ld否则config过程会包连接错误,
 # 只能通过armv7a-linux-androideabi-clang来连接
@@ -102,22 +126,22 @@ FF_CFG_FLAGS="$FF_CFG_FLAGS --enable-iconv"
 FF_CFG_FLAGS="$FF_CFG_FLAGS --enable-libass"
 FF_CFG_FLAGS="$FF_CFG_FLAGS --enable-librtmp"
 
-
 FF_CFG_FLAGS="$FF_CFG_FLAGS --enable-gpl"
-
 FF_CFG_FLAGS="$FF_CFG_FLAGS --enable-nonfree"
 FF_CFG_FLAGS="$FF_CFG_FLAGS --disable-postproc"
 FF_CFG_FLAGS="$FF_CFG_FLAGS --disable-avdevice"
 FF_CFG_FLAGS="$FF_CFG_FLAGS --enable-protocol=rtp"
 FF_CFG_FLAGS="$FF_CFG_FLAGS --enable-demuxer=rtsp"
 FF_CFG_FLAGS="$FF_CFG_FLAGS --enable-protocol=tcp"
+FF_CFG_FLAGS="$FF_CFG_FLAGS --disable-asm"
 
 
 FF_CFLAGS="$PREFIX/include"
-FF_DEP_LIBS=
+# 必须添加-lm和-lz的依赖
+FF_DEP_LIBS="-lm -lz -L$PREFIX/lib"
 
 # 必须添加-lm和-lz的依赖
-FF_DEP_LIBS="-lm -lz -L$PREFIX/lib -lmp3lame -lxml2 -lpng16 -lharfbuzz -lfreetype -liconv -lfribidi -lfontconfig"
+FF_DEP_LIBS="$FF_DEP_LIBS -lmp3lame -lxml2 -lpng16 -lharfbuzz -lfreetype -liconv -lfribidi -lfontconfig"
 
 # FF_CFLAGS="-O3 -DANDROID"
 # FF_CFLAGS="$FF_CFLAGS -I${FF_DEP_OPENSSL_INC} -I${FF_DEP_RTMP_INC}"
@@ -143,12 +167,14 @@ echo $FF_DEP_LIBS
 echo $FF_EXTRA_LDFLAGS
 echo $SYSROOT
 
-# pkg-config libssl --libs
+#pkg-config libass --libs
+#pkg-config freetype2 --libs
+#pkg-config harfbuzz --libs
+
+#exit 0
 ./configure $FF_CFG_FLAGS --prefix=$PREFIX \
 --cc=$CC \
 --cxx=$CXX \
---arch=$ARCH \
---cpu=$CPU \
 --sysroot=$SYSROOT \
 --pkg-config=pkg-config \
 --extra-cflags="-fpic $OPTIMIZE_CFLAGS $FF_CFLAGS" \
@@ -162,6 +188,13 @@ echo $SYSROOT
 # --enable-shared \
 # --nm=$NM \
 # --ar=$AR \
+#exit 0
+if [ $? -ne 0 ];then
+    echo "configure ffmpeg error!"
+    exit 1
+fi
 
-make
-make install
+make || exit 1
+make install || exit 1
+
+
