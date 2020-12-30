f5e68f0406dfbf61386deb9708fc7a7bfa9e94d3
jack <ytxhao@gmail.com>
2020-12-30 16:51:04 +0800

    update fontconfig build file

--- android_libs/contrib/libfontconfig/fontconfig-2.13.91/src/fcstdint.h
+++ android_libs/contrib/libfontconfig/fontconfig-2.13.91/src/fcstdint.h
@@ -2,7 +2,7 @@
 #define _FONTCONFIG_SRC_FCSTDINT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H "fontconfig 2.13.91"
-/* generated using gnu compiler Android (6454773 based on r365631c2) clang version 9.0.8 (https://android.googlesource.com/toolchain/llvm-project 98c855489587874b2a325e7a516b99d838599c6f) (based on LLVM 9.0.8svn) */
+/* generated using gnu compiler gcc (GCC) 9.1.1 20190503 (Red Hat 9.1.1-1) */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
